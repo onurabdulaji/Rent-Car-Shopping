deef215c8f921fa99ec4c984079f4b06a6196345
Onur Abdulaji <onurabdulaji@gmail.com>
2024-11-19 10:16:15 +0100

    Add project files.

---
+++ Rent-Car-Shopping/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Rent_Car_Shopping.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
